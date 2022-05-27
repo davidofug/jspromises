c3e757839358b0ff1f96a164c1c532a91ddd61b6
David Wampamba <davidwampamba@gmail.com>
2022-05-27 13:35:04 +0300

    Integrate CSS and implement Promise to fetch text from a file

--- assets/scripts/promises.js
+++ assets/scripts/promises.js
@@ -17,3 +17,20 @@ orderFood
 	.catch((error) => {
 		console.error(error);
 	});
+
+const LEFT = document.getElementById("left");
+const ARTICLE = document.getElementById("article");
+const RIGHT = document.getElementById("right");
+
+const leftPromise = new Promise((resolve, reject) => {
+	fetch("../assets/data/left.txt")
+		.then((response) => {
+			return response.text();
+		})
+		.then((text) => resolve(text))
+		.catch((error) => reject(error));
+});
+
+leftPromise
+	.then((text) => (LEFT.innerText = text))
+	.catch((error) => console.error(error));
